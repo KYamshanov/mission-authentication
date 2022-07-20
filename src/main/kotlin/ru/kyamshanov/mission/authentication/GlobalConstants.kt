4b1930f0cf750a4127ac40bf9dfb86bf7e913304
KYamshanov <undframe@gmail.com>
2022-07-20 17:50:59 +0400

    Добавление тестового контролера

--- src/main/kotlin/ru/kyamshanov/mission/authentication/GlobalConstants.kt
+++ src/main/kotlin/ru/kyamshanov/mission/authentication/GlobalConstants.kt
@@ -3,10 +3,15 @@ package ru.kyamshanov.mission.authentication
 /**
  * Основные константы приложения
  */
-object Constants {
+object GlobalConstants {
 
     /**
      * Источник сертификатов приложения
      */
     const val CERTIFICATES_PROPERTY_SOURCE = "classpath:certificates.properties"
+
+    /**
+     * Ключ для тогла - включен ли тестовый контроллер
+     */
+    const val KEY_ENABLED_TEST_CONTROLLER = "controller.test.enabled"
 }
---
+++ src/main/kotlin/ru/kyamshanov/mission/authentication/controllers/StatusCodeConstants.kt
@@ -0,0 +1,13 @@
+package ru.kyamshanov.mission.authentication.controllers
+
+/**
+ * Константы для статус-кодов ответов
+ */
+object StatusCodeConstants {
+
+    /**
+     * Успешно
+     */
+    const val SUCCESS_STATUS_CODE = 200
+
+}
