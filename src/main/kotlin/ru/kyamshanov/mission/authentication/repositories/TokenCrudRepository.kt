3d7025692ba34b063a2f9a5c74b8291f945d423a
Ямшанов Константин Алексеевич (4059153) <yamshanov@dev.vtb.ru>
2022-09-02 10:54:18 +0400

    Планировщики на удаление неактивных токенов

--- src/main/kotlin/ru/kyamshanov/mission/authentication/repositories/TokenCrudRepository.kt
+++ src/main/kotlin/ru/kyamshanov/mission/authentication/repositories/TokenCrudRepository.kt
@@ -1,9 +1,20 @@
 package ru.kyamshanov.mission.authentication.repositories
 
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import ru.kyamshanov.mission.authentication.entities.TokenEntity
+import java.time.LocalDateTime
 
 /**
  * CRUD репозиторий для хранения сущнстей сессионных токен
  */
-internal interface TokenCrudRepository : CoroutineCrudRepository<TokenEntity, String>
+internal interface TokenCrudRepository : CoroutineCrudRepository<TokenEntity, String> {
+
+
+    /**
+     * Удалить токены с истекшим сроком действия
+     * @param expiresAt Минимальная дата истечения скрока дейтсивя
+     */
+    @Query("DELETE FROM mission.public.auth_tokens WHERE refresh_expires_at <= :date")
+    suspend fun deleteExpiredTokens(expiresAt: LocalDateTime)
+}
---
+++ src/main/kotlin/ru/kyamshanov/mission/authentication/schedulers/ClearExpiredSessionsScheduledTask.kt
@@ -0,0 +1,36 @@
+package ru.kyamshanov.mission.authentication.schedulers
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import ru.kyamshanov.mission.authentication.repositories.TokenSafeRepository
+import java.util.concurrent.TimeUnit
+
+/**
+ * Планируемая задача по удалению токенов и истекшим сроком действия
+ */
+@Component
+internal class ClearExpiredSessionsScheduledTask(
+    private val safeRepository: TokenSafeRepository
+) {
+
+    private val componentCoroutineScope = CoroutineScope(Dispatchers.IO)
+
+    @Scheduled(fixedRate = 30, timeUnit = TimeUnit.SECONDS)
+    fun clearSessionTokens() {
+        componentCoroutineScope.launch {
+            println("Deleting tokens")
+            safeRepository.deleteExpiredTokens()
+        }
+    }
+
+    @Scheduled(fixedRate = 30, timeUnit = TimeUnit.SECONDS)
+    fun clearBlockedAccessTokens() {
+        componentCoroutineScope.launch {
+            println("Deleting tokens")
+            safeRepository.deleteExpiredTokens()
+        }
+    }
+}
